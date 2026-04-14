53b670a836291295e5ab637650f5ae7075d70d75
zemaria83 <ze.maria83@gmail.com>
2026-04-14 12:06:13 +0100

    atualizei alloed hosts

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4t@*9jt44k(br3h_*ju2b92a&4k8j$l0=0+jjpy8dsq8erfmrw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-intro-9pv2.onrender.com']
 
 
 # Application definition
